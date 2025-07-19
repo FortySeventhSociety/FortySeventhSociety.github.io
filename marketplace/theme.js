880e0e3e7da329fdf8b377ec2ce0c6231bca51b7
D474designs <jcazares117@gmail.com>
2025-07-19 03:21:17 -0700

    update site

--- marketplace/theme.js
+++ marketplace/theme.js
@@ -1,35 +1,35 @@
 // D474designs | Modifications, and fixes by JOCV-III ///////
 // All Rights Reserved ///////
 
-        // Funzione per cambiare il CSS
-        function changeCSS(cssFile) {
-            var link = document.getElementById("theme-stylesheet");
+// Funzione per cambiare il CSS
+function changeCSS(cssFile) {
+    var link = document.getElementById("theme-stylesheet");
 
-            if (!link) {
-                link = document.createElement("link");
-                link.rel = "stylesheet";
-                link.id = "theme-stylesheet";
-                document.head.appendChild(link);
-            }
+    if (!link) {
+        link = document.createElement("link");
+        link.rel = "stylesheet";
+        link.id = "theme-stylesheet";
+        document.head.appendChild(link);
+    }
 
-            link.href = cssFile;
+    link.href = cssFile;
 
-            // Memorizza la scelta dell'utente nel localStorage
-            localStorage.setItem('selectedCSS', cssFile);
-        }
+    // Memorizza la scelta dell'utente nel localStorage
+    localStorage.setItem("selectedCSS", cssFile);
+}
 
-        // Ripristina la scelta dell'utente dal localStorage
-        function restoreCSS() {
-            var selectedCSS = localStorage.getItem('selectedCSS') || 'css/default.css';
-            var selectElement = document.getElementById('css-selector');
+// Ripristina la scelta dell'utente dal localStorage
+function restoreCSS() {
+    var selectedCSS = localStorage.getItem("selectedCSS") || "css/default.css";
+    var selectElement = document.getElementById("css-selector");
 
-            if (selectElement) {
-                selectElement.value = selectedCSS;
-                changeCSS(selectedCSS);
-            }
-        }
+    if (selectElement) {
+        selectElement.value = selectedCSS;
+        changeCSS(selectedCSS);
+    }
+}
 
-        /*
+/*
         setTimeout(function() {
                 $(".dt-input").focus();
         }, 1000);
@@ -51,14 +51,14 @@
         }, 10000);
             */
 
-        /*
+/*
         setTimeout(function() {
         const searchInput = document.getElementById('dt-search-0');
             searchInput.focus();
         }, 5000);
         */
 
-        /*
+/*
         // JavaScript to autofocus the input field without scrolling or interfering with typing.
         // The 'DOMContentLoaded' event ensures the script runs only after the entire HTML
         // document has been completely loaded and parsed, without waiting for stylesheets, images, and subframes to finish loading.
@@ -76,18 +76,20 @@
         });
         */
 
-        async function fetchDataAndProcess() {
-            try {
-                const response = await fetch('https://api.nomstead.com/open/marketplace');
-                setTimeout(function() {
-                    $('.dt-input').focus();
-                }, 5000);
-            } catch (error) {
-                console.error('Error fetching data:', error);
-            }
-        }
+async function fetchDataAndProcess() {
+    try {
+        const response = await fetch(
+            "https://api.nomstead.com/open/marketplace",
+        );
+        setTimeout(function () {
+            $(".dt-input").focus();
+        }, 5000);
+    } catch (error) {
+        console.error("Error fetching data:", error);
+    }
+}
 
-        // Ripristina il CSS quando la pagina viene caricata
-        window.onload = restoreCSS;
+// Ripristina il CSS quando la pagina viene caricata
+window.onload = restoreCSS;
 
-        fetchDataAndProcess();
+fetchDataAndProcess();
